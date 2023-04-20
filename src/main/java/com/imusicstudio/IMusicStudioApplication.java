d74c4db5ac5c2e0e18ca89de1220d786dfd1e582
tranvansy <sytranvan220501@gmail.com>
2023-04-20 14:16:56 +0700

    add setup project

---
+++ src/main/java/com/imusicstudio/IMusicStudioApplication.java
@@ -0,0 +1,13 @@
+package com.imusicstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMusicStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IMusicStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imusicstudio/IMusicStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imusicstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMusicStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
