b643bfa9ffa9b2527436227addac850893d66414
Quang292001 <nh.quang29200189@gmail.com>
2023-05-09 19:10:44 +0700

    "cap nhat lan 5"

--- src/main/java/com/imusicstudio/controller/admin/AdminController.java
+++ src/main/java/com/imusicstudio/controller/admin/AdminController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
+@Controller
 public class AdminController {
 	@RequestMapping(value = { "/admin"}, method = RequestMethod.GET)
 	public ModelAndView homePage() {
