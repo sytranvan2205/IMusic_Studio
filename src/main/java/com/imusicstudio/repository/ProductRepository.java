35bf0d3269e7d02acae7b739e12beac3c03e95da
taipham <85749750+manhtuenhan@users.noreply.github.com>
2023-05-12 17:40:10 +0700

    add ProductDetail

--- src/main/java/com/imusicstudio/repository/ProductRepository.java
+++ src/main/java/com/imusicstudio/repository/ProductRepository.java
@@ -3,9 +3,9 @@ package com.imusicstudio.repository;
 import com.imusicstudio.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductsRepository extends JpaRepository<Product,String> {
+public interface ProductRepository extends JpaRepository<Product,String> {
 
     // Tìm sàn phẩm trong tìm kiếm
 
-
+    Product findById(long Id);
 }
